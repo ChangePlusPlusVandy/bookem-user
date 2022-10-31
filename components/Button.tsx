1f6e8081ae97ccd4c469a9fda09714ec3dd0b876
Zi Nean Teoh <zinean00@gmail.com>
2022-10-31 13:26:00 -0500

    add paths to make importing components & styles easier

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => {
+  return <button>Demo Button</button>;
+};
+
+export default Button;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import Button from '@/components/Button';
+import styles from '@/styles/Home.module.css';
+
 export default function Home() {
   return (
-    <div>Hello curious devs! This is what we will be building together...</div>
+    <>
+      <div className={styles.container}>
+        Hello curious devs! This is what we will be building together...
+      </div>
+      <Button />
+    </>
   );
 }
