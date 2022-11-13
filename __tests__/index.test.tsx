6230a1bad2f388ea71915157d4b7958642ff7d52
Zi Nean Teoh <zinean00@gmail.com>
2022-11-13 02:01:49 -0600

    update jest tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,13 +1,39 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { useSession } from 'next-auth/react';
 import Home from '@/pages/index';
+jest.mock('next-auth/react');
 
 describe('Home', () => {
-  it('renders correctly', () => {
-    // renders the component
-    render(<Home />);
+  it('login page rendered correctly', () => {
+    // Mock useSession to return a logged in user
+    const mockSession = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: { username: 'admin' },
+    };
+    (useSession as jest.Mock).mockReturnValueOnce([
+      mockSession,
+      'authenticated',
+    ]);
 
-    // expect the screen now to have a button
-    expect(screen.getByRole('button')).toBeInTheDocument();
+    // renders the component
+    const { container } = render(<Home />);
+
+    // expect the screen now to have a login message
+    expect(screen.getByText('Logging you in')).toBeInTheDocument();
+
+    // expect the screen to have a form tag
+    expect(container.querySelector('form')).toBeInTheDocument();
+
+    // expect the screen to have 3 input elements (email, password, submit)
+    expect(container.querySelectorAll('input').length).toBe(3);
+
+    // expect password input to be of type password
+    expect(
+      container.querySelector('input[type="password"]')
+    ).toBeInTheDocument();
+
+    // expect the screen to have a submit button
+    expect(container.querySelector('input[type="submit"]')).toBeInTheDocument();
   });
 });
