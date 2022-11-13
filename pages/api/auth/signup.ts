a4cdae4437c4fefdfdea2e0379cd6f1eb2eea4e6
Zi Nean Teoh <zinean00@gmail.com>
2022-11-13 01:35:02 -0600

    add register endpoint and create interface file

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,50 @@
+import dbConnect from 'lib/dbConnect';
+import { hash } from 'bcrypt';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Users from 'models/Users';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  switch (req.method) {
+    case 'POST':
+      // Get the user's email and password from the request body
+      const { email, password, name } = req.body;
+
+      // Check if the user's email and password are valid
+      if (!email || !email.includes('@') || !password) {
+        res.status(422).json({ message: 'Invalid input' });
+        throw new Error('Invalid input');
+      }
+
+      // Connect to the database
+      await dbConnect();
+
+      // Check if the user already exists in database
+      const checkExisting = await Users.findOne({ email });
+
+      // If the user already exists, return an error
+      if (checkExisting) {
+        res.status(422).json({ message: 'User email already exists' });
+        throw new Error('User email already exists');
+      }
+
+      // Hash the user's password
+      const hashedPassword = await hash(password, 12);
+
+      // Create a new user in the database
+      const status = await Users.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      // Return the status of the user creation
+      res.status(201).json({ message: 'User created', ...status });
+      break;
+    default:
+      res.status(400).json({ message: 'Invalid request method' });
+      break;
+  }
+}
