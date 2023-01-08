cdb10b0e301100b0f12bbd81d378919cfb067d4a
Zi Nean Teoh <zinean00@gmail.com>
2023-01-08 08:50:26 -0600

    add frontend and backend tests

---
+++ __tests__/backend/index.test.ts
@@ -0,0 +1,7 @@
+import handler from '../../pages/api/volunteerLogs';
+
+describe('Backend when unauthenticated', () => {
+  it('should return a 200 status code', async () => {
+    // TODO:
+  });
+});
