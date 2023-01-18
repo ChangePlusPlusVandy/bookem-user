4565b3d977c723f9b8905d4a84815843726bdcbf
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2023-01-17 18:47:20 -0600

    Create main parts of future volunteer events page

--- components/FutureVolunteerEvents.tsx
+++ components/FutureVolunteerEvents.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
+type EventType = {
+  source: string;
+  name: string;
+  location: string;
+  date: string;
+  time: string;
+  availability: string;
+  id: number;
+};
+
 const Container = styled.div`
   background-color: pink;
   border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
@@ -59,6 +69,126 @@ const Input = styled.input`
   }
 `;
 
+const ImgContainer = styled.div`
+  position: relative;
+  flex-basis: 50%;
+  flex-basis: calc(20.333%);
+  margin: 40px;
+  cursor: pointer;
+  align-items: center;
+`;
+
+const ImgIcons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  margin-right: 20px;
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+const ImgMeta = styled.div`
+  display: none;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const Img = styled.img`
+  cursor: pointer;
+  width: 100%;
+  background-color: yellow;
+  padding: 80px;
+  padding-top: 20px;
+  border-radius: 7px;
+  border: 1px solid #6b6b6b;
+`;
+
+function Events({ event }: { event: EventType }) {
+  return (
+    <ImgContainer>
+      <Img src={event.source} />
+      <ImgMeta>
+        <ImgIcons>
+          <Image
+            src="/locationIcon.png"
+            alt="Location icon"
+            width="25"
+            height="25"
+          />
+          {event.location}
+        </ImgIcons>
+        <ImgIcons>{event.time}</ImgIcons>
+      </ImgMeta>
+    </ImgContainer>
+  );
+}
+
+const feedsource: EventType[] = [
+  {
+    source: '/eventIcon.png',
+    name: 'Distribute books (BNFK)',
+    location: '3593 Cedar Rd. Nashville',
+    date: '11/25',
+    time: '9:30 AM',
+    availability: '11 spots',
+    id: 0,
+  },
+  {
+    source: '/eventIcon.png',
+    name: 'Distribute books (BNFK)',
+    location: '3593 Cedar Rd. Nashville',
+    date: '11/25',
+    time: '9:30 AM',
+    availability: '11 spots',
+    id: 1,
+  },
+  {
+    source: '/eventIcon.png',
+    name: 'Distribute books (BNFK)',
+    location: '3593 Cedar Rd. Nashville',
+    date: '11/25',
+    time: '9:30 AM',
+    availability: '11 spots',
+    id: 2,
+  },
+  {
+    source: '/eventIcon.png',
+    name: 'Distribute books (BNFK)',
+    location: '3593 Cedar Rd. Nashville',
+    date: '11/25',
+    time: '9:30 AM',
+    availability: '11 spots',
+    id: 3,
+  },
+  {
+    source: '/eventIcon.png',
+    name: 'Distribute books (BNFK)',
+    location: '3593 Cedar Rd. Nashville',
+    date: '11/25',
+    time: '9:30 AM',
+    availability: '11 spots',
+    id: 4,
+  },
+];
+
+const ImagesWrapper = styled.div`
+  background-color: orange;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  width: 60%;
+  justify-content: center;
+  margin: auto;
+`;
+
 const FutureVolunteerEvents = () => {
   return (
     <Container>
@@ -77,6 +207,12 @@ const FutureVolunteerEvents = () => {
       <SearchBar>
         <Input type="text" placeholder="Search" />
       </SearchBar>
+
+      <ImagesWrapper>
+        {feedsource.map(item => (
+          <Events event={item} key={item.id} />
+        ))}
+      </ImagesWrapper>
     </Container>
   );
 };
