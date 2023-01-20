a2903d3d1c51ca91f90fad356add338296aa19de
Zi Nean Teoh <zinean00@gmail.com>
2023-01-20 14:14:38 -0600

    refactor main dashboard
    Co-authored-by: Sophia Brent <sophiaebrent@gmail.com>

--- components/Home/MainDashboard.tsx
+++ components/Home/MainDashboard.tsx
@@ -2,91 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import UpcomingEvents from './UpcomingEvents';
 import Image from 'next/image';
-
-const Container = styled.div`
-  height: 100%;
-  width: 100%;
-  background-color: white;
-  padding: 40px;
-`;
-
-const GreetingContainer = styled.div`
-  color: lightblue;
-  height: fit-content;
-  background-color: white;
-`;
-
-const Greeting = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 40px;
-  line-height: 48px;
-  color: black;
-`;
-
-const InfoIcon = styled.div`
-  position: absolute;
-  top: 82px;
-  right: 50px;
-`;
-
-const StatsContainer = styled.div`
-  height: fit-content;
-  margin-top: 49px;
-  background-color: white;
-`;
-
-const StatsHeader = styled.p`
-  margin-bottom: 29px;
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 25px;
-  line-height: 30px;
-  color: #000000;
-`;
-
-const StatsFlex = styled.div`
-  height: fit-content;
-  background-color: white;
-  display: flex;
-  justify-content: space-evenly;
-`;
-
-const StatsFlexChild = styled.div`
-  flex: 1;
-  background-color: white;
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  vertical-align: middle;
-`;
-
-const StatsNumber = styled.p`
-  margin: 0;
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 40px;
-  line-height: 48px;
-  color: #000000;
-`;
-
-const StatsDescription = styled.p`
-  margin: 0;
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 19px;
-  text-align: center;
-  color: #000000;
-`;
-
-const UpcomingEventsContainer = styled.div`
-  margin-top: 40px;
-`;
+import {
+  Container,
+  Greeting,
+  GreetingContainer,
+  InfoIcon,
+  StatsContainer,
+  StatsDescription,
+  StatsFlex,
+  StatsFlexChild,
+  StatsHeader,
+  StatsNumber,
+  UpcomingEventsContainer,
+} from '@/styles/dashboard.styles';
 
 const MainDashboard = ({ userData }: any) => {
   return (
