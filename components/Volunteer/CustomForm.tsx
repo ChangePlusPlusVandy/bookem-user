5adc97d42adc3f94ebbadc0cdfb6e9facc51f22f
Sebastian <sstull1842@gmail.com>
2023-01-23 13:14:10 -0600

    Finish prototype of form

--- components/Volunteer/CustomForm.tsx
+++ components/Volunteer/CustomForm.tsx
@@ -20,6 +20,34 @@ const CustomForm = () => {
     flex-wrap: wrap;
     flex-direction: column;
     justify-content: space-between;
+    margin: 10px 0;
+  `;
+
+  const FormLabel = styled.label`
+    margin-top: 20px;
+  `;
+
+  const FormInput = styled.input`
+    margin-top: 20px;
+    border-radius: 10px;
+    height: 25px;
+  `;
+
+  const LargeFormInput = styled.textarea`
+    margin-top: 20px;
+    border-radius: 10px;
+    width: 400px;
+    height: 100px;
+    font-family: 'Times New Roman', serif;
+    font-size: 1em;
+  `;
+
+  const SubmitButton = styled.input`
+    margin-left: 150px;
+    margin-top: 40px;
+    width: 100px;
+    height: 40px;
+    border-radius: 10px;
   `;
 
   return (
@@ -27,27 +55,40 @@ const CustomForm = () => {
       <FormBox>
         <List>
           <FormHeader>Please select one program</FormHeader>
-          <label>
-            <input
-              type="radio"
-              name="first-page"
-              value="Reading is Fundamental (RIF)"></input>
+          <FormLabel>
+            <input type="radio" name="first-page"></input>
             Reading is Fundamental (RIF)
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="first-page"
-              value="Ready for Reading (RFR)"></input>
+          </FormLabel>
+          <FormLabel>
+            <input type="radio" name="first-page"></input>
             Ready for Reading (RFR)
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="first-page"
-              value="Books for Nashville Kids (BFNK)"></input>
+          </FormLabel>
+          <FormLabel>
+            <input type="radio" name="first-page"></input>
             Books for Nashville Kids (BFNK)
-          </label>
+          </FormLabel>
+          <FormLabel>Please log volunteer hours</FormLabel>
+          <FormInput type="text" placeholder="# of hours"></FormInput>
+          <FormLabel>Date of visit</FormLabel>
+          <FormInput type="text" placeholder="MM/DD/YYYY"></FormInput>
+          <FormLabel>How many books did you help distribute, if any?</FormLabel>
+          <FormInput type="text" placeholder="# of books"></FormInput>
+          <FormLabel>Anything else you&apos;d like to share?</FormLabel>
+          <LargeFormInput placeholder="Comment here..."></LargeFormInput>
+          <FormLabel>Would you like to share this comment?</FormLabel>
+          <FormLabel>
+            <input type="radio" name="third-page"></input>
+            Yes
+          </FormLabel>
+          <FormLabel>
+            <input type="radio" name="third-page"></input>
+            Yes, but anonymously
+          </FormLabel>
+          <FormLabel>
+            <input type="radio" name="third-page"></input>
+            No
+          </FormLabel>
+          <SubmitButton type="submit"></SubmitButton>
         </List>
       </FormBox>
     </form>
