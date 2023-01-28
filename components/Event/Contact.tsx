daaabf475afda4756584176c5e42e58d9fc94912
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-01-27 19:11:32 -0600

    Basic structure of Contact section

--- components/Event/Contact.tsx
+++ components/Event/Contact.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -8,10 +9,52 @@ const ContactBox = styled.div`
   background-color: #d2aae6;
 `;
 
+const ContactHeader = styled.div``;
+
+const IconBox = styled.div``;
+
+const Icon = styled(Image)``;
+
+const IconText = styled.span``;
+
+interface IconParams {
+  src: string;
+  text: string;
+}
+
 const Contact = () => {
+  const iconParamList: IconParams[] = [
+    {
+      src: '/event/phone.png',
+      text: '615-383-3837',
+    },
+    {
+      src: '/event/mail.png',
+      text: 'bookem@gmail.com',
+    },
+  ];
+
+  const iconWidth = 23;
+  const iconHeight = 23;
   return (
     <>
-      <ContactBox></ContactBox>
+      <ContactBox>
+        <ContactHeader></ContactHeader>
+
+        {iconParamList.map(iconParam => {
+          return (
+            <IconBox key={iconParam.src}>
+              <Icon
+                src={iconParam.src}
+                alt=""
+                width={iconWidth}
+                height={iconHeight}
+              />
+              <IconText>{iconParam.text}</IconText>
+            </IconBox>
+          );
+        })}
+      </ContactBox>
     </>
   );
 };
