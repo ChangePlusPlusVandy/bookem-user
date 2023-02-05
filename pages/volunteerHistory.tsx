4fd0f8e8dde89ff76e45dcef146c3e699019b753
Sebastian <sstull1842@gmail.com>
2023-02-05 15:35:58 -0600

    Add functional back arrow

--- pages/volunteerHistory.tsx
+++ pages/volunteerHistory.tsx
@@ -2,13 +2,20 @@ import LongEventCard from '@/components/LongEventCard';
 import React from 'react';
 import styled from 'styled-components';
 import { dummyEventData } from '../components/Home/UpcomingEvents';
+import Image from 'next/image';
 
 const Header = styled.h2`
   font-family: 'Inter';
   font-size: 25px;
   margin-top: 50px;
-  margin-left 100px;
+  margin-left: 100px;
   font-weight: 400;
+`;
+
+const BackArrow = styled.button`
+  position: absolute;
+  margin-top: 50px;
+  margin-left: 25px;
 `;
 
 const MainContainer = styled.div`
@@ -26,6 +33,9 @@ const MainContainer = styled.div`
 const VolunteerHistoryPage = () => {
   return (
     <>
+      <BackArrow onClick={event => (window.location.href = '/volunteer')}>
+        <Image src="/back-arrow.png" alt="Go Back" height="20" width="20" />
+      </BackArrow>
       <Header>Volunteer History</Header>
       <MainContainer>
         <LongEventCard eventData={dummyEventData} size="medium"></LongEventCard>
