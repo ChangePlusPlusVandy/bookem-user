7b39374b679ecb3d49c754212534f1caf9ce2104
Carol He <carolhe.che.17@gmail.com>
2023-02-07 19:31:28 -0600

    add old unstyled components and autoformat of birthday and phone

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
 import { UserData } from 'bookem-shared/src/types/database';
 
+const Header = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const NextButton = styled.input``;
+
+// adapted from https://tomduffytech.com/how-to-format-phone-number-in-react/
+const formatPhoneNumber = (value: string) => {
+  // if input value is falsy eg if the user deletes the input, then just return
+  if (!value) return value;
+
+  // clean the input for any non-digit values.
+  const phoneNumber: string = value.replace(/[^\d]/g, '');
+
+  // phoneNumberLength is used to know when to apply our formatting for the phone number
+  const phoneNumberLength: number = phoneNumber.length;
+
+  // we need to return the value with no formatting if its less then four digits
+  // this is to avoid weird behavior that occurs if you  format the area code to early
+  if (phoneNumberLength < 4) return phoneNumber;
+
+  // if phoneNumberLength is greater than 4 and less the 7 we start to return
+  // the formatted number
+  if (phoneNumberLength < 7) {
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+  }
+
+  // finally, if the phoneNumberLength is greater then seven, we add the last
+  // bit of formatting and return it.
+  return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+    3,
+    6
+  )}-${phoneNumber.slice(6, 10)}`;
+};
+
+const formatBirthday = (value: string) => {
+  if (!value) return value;
+
+  const birthday: string = value.replace(/[^\d]/g, '');
+
+  const birthdayLength: number = birthday.length;
+
+  if (birthdayLength == 1) return birthday;
+
+  if (birthdayLength == 2) {
+    return `${birthday.slice(0, 2)}-`;
+  }
+
+  if (birthdayLength == 3) {
+    return `${birthday.slice(0, 2)}-${birthday.slice(2)}`;
+  }
+
+  if (birthdayLength == 4) {
+    return `${birthday.slice(0, 2)}-${birthday.slice(2)}-`;
+  }
+
+  return `${birthday.slice(0, 2)}-${birthday.slice(2, 4)}-${birthday.slice(
+    4,
+    8
+  )}`;
+};
+
 const RegisterPage = () => {
+  // phone number handling
+  const [phoneValue, setPhoneValue] = useState('');
+  const handlePhone = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+    setPhoneValue(formattedPhoneNumber);
+  };
+
+  // birthday handling
+  const [birthdayValue, setBirthdayValue] = useState('');
+  const handleBirthday = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedBirthday = formatBirthday(e.target.value);
+    setBirthdayValue(formattedBirthday);
+  };
+
+  // form handling
   const {
     register,
     handleSubmit,
@@ -44,48 +124,59 @@ const RegisterPage = () => {
     }
   };
 
-  return (
-    <>
-      <div>Tell us about yourself!</div>
-      <form id="registerPage1" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('firstName', { required: true })}
-          placeholder="First name"></input>
-        <input
-          {...register('lastName', { required: true })}
-          placeholder="Last name"></input>
-        <input
-          {...register('phone', { required: true })}
-          placeholder="Phone number"></input>
-        <input
-          {...register('email', { required: true })}
-          placeholder="Email Address"></input>
-        <input
-          {...register('streetAddress', { required: true })}
-          placeholder="Street address"></input>
-        <input
-          {...register('city', { required: true })}
-          placeholder="City"></input>
-        <input
-          {...register('state', { required: true })}
-          placeholder="State"></input>
-        <input
-          {...register('zip', { required: true })}
-          placeholder="Zip code"></input>
+  // registration page select
+  const page = 1;
+  if (page == 1)
+    return (
+      <>
+        <Header>Tell us about yourself!</Header>
+        <Form id="registerPage1" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            {...register('firstName', { required: true })}
+            placeholder="First name"></Input>
+          <Input
+            {...register('lastName', { required: true })}
+            placeholder="Last name"></Input>
+          <Input
+            {...register('birthday', { required: true })}
+            placeholder="Date of birth (MM-DD-YYY)"
+            onChange={e => handleBirthday(e)}
+            value={birthdayValue}></Input>
+          <Input
+            {...register('phone', { required: true })}
+            placeholder="Phone number"
+            onChange={e => handlePhone(e)}
+            value={phoneValue}></Input>
+          <Input
+            {...register('email', { required: true })}
+            placeholder="Email Address"></Input>
+          <Input
+            {...register('streetAddress', { required: true })}
+            placeholder="Street address"></Input>
+          <Input
+            {...register('city', { required: true })}
+            placeholder="City"></Input>
+          <Input
+            {...register('state', { required: true })}
+            placeholder="State"></Input>
+          <Input
+            {...register('zip', { required: true })}
+            placeholder="Zip code"></Input>
 
-        {errors.firstName && <span>First name is required</span>}
-        {errors.lastName && <span>Last name is required</span>}
-        {errors.phone && <span>Phone number is required</span>}
-        {errors.email && <span>Email address is required</span>}
-        {errors.streetAddress && <span>Street address is required</span>}
-        {errors.city && <span>City is required</span>}
-        {errors.state && <span>State is required</span>}
-        {errors.zip && <span>Zip code is required</span>}
+          {errors.firstName && <span>First name is required</span>}
+          {errors.lastName && <span>Last name is required</span>}
+          {errors.birthday && <span>Date of birth is required</span>}
+          {errors.phone && <span>Phone number is required</span>}
+          {errors.email && <span>Email address is required</span>}
+          {errors.streetAddress && <span>Street address is required</span>}
+          {errors.city && <span>City is required</span>}
+          {errors.state && <span>State is required</span>}
+          {errors.zip && <span>Zip code is required</span>}
 
-        <input form="registerPage1" type="submit" value="" />
-      </form>
-    </>
-  );
+          <NextButton form="registerPage1" type="submit" value="" />
+        </Form>
+      </>
+    );
 };
 
 export default RegisterPage;
