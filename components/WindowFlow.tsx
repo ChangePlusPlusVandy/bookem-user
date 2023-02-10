edf8f23010f0665acbd970497825df3245a47fa6
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2023-02-10 00:01:05 -0600

    Styled window flow

--- components/WindowFlow.tsx
+++ components/WindowFlow.tsx
@@ -9,8 +9,13 @@ const PageNum = styled.div`
   border: solid 1px black;
   height: 50px;
   width: 50px;
-  margin: 0 auto;
+  margin: 0 10px 10px 10px;
   padding-left: 20px;
+  padding-top: 15px;
+  display: flex;
+`;
+
+const PageTitle = styled.div`
   padding-top: 15px;
 `;
 
@@ -19,44 +24,47 @@ const PageNumHeader = styled.div`
   margin: 0 auto;
   margin-top: 15px;
   width: 90%;
+  justify-content: center;
 `;
 
-const ButtonNav = styled.div`
-  width: calc(100% - 150px);
+const ButtonRight = styled.div`
   bottom: 30px;
   position: absolute;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  justify-content: space-between;
-  display: flex;
+  right: 30px;
+`;
+const ButtonLeft = styled.div`
+  bottom: 30px;
+  position: absolute;
+  left: 30px;
 `;
 
 const Button = styled.button`
   background: transparent;
   border: transparent;
   cursor: pointer;
-  background-color: pink;
   border-radius: 50%;
   border: 2px solid black;
   padding: 0 2px;
 `;
 
+const ImageWrapper = styled.div`
+  padding-top: 10px;
+  padding-left: 20px;
+`;
+
 const WindowFlow = ({ children }: { children: React.ReactNode }) => {
   // a variable for the page header alongside names for the pages
   // this is an input into this Windowflow component
-  const pages = ['Event', 'Program', 'Numbers', 'Comments', 'Hi'];
+  const pages = ['Event', 'Program', 'More Information'];
   const numPages = pages.length;
 
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    // figure out if we pressed increment / decrement
     // if it's increment, then we only need to change the lement at index and index + 1
     // if decrement then we only need to change the element at index - 1 and index
-    // TODO:
     //
-    // // for each of our pages
+    // for each of our pages
     pages.forEach((page, index) => {
       // get the document element
       const currPage = document.getElementById((index + 1).toString());
@@ -93,29 +101,50 @@ const WindowFlow = ({ children }: { children: React.ReactNode }) => {
     <Container>
       <PageNumHeader>
         {pages.map((page, index) => {
-          return <PageNum id={(index + 1).toString()}>{index + 1}</PageNum>;
+          return (
+            <>
+              <PageNum id={(index + 1).toString()}>{index + 1}</PageNum>
+              <PageTitle>{pages[index]}</PageTitle>
+
+              {index < pages.length - 1 ? (
+                <ImageWrapper>
+                  <Image
+                    src="/line.png"
+                    alt="Line between page titles"
+                    width="40"
+                    height="1"
+                  />
+                </ImageWrapper>
+              ) : null}
+
+              <ButtonLeft>
+                {currentPage > 1 ? (
+                  <Button onClick={navigateBack}>
+                    <Image
+                      src="/arrow-left.png"
+                      alt="Left arrow"
+                      width="40"
+                      height="40"
+                    />
+                  </Button>
+                ) : null}
+              </ButtonLeft>
+              <ButtonRight>
+                {currentPage < pages.length ? (
+                  <Button onClick={navigateForward}>
+                    <Image
+                      src="/arrow-right.png"
+                      alt="Right arrow"
+                      width="40"
+                      height="40"
+                    />
+                  </Button>
+                ) : null}
+              </ButtonRight>
+            </>
+          );
         })}
       </PageNumHeader>
-      This is current page: {currentPage}
-      <ButtonNav>
-        <Button onClick={navigateBack}>
-          <Image
-            src="/arrow-left.png"
-            alt="Left arrow"
-            width="40"
-            height="40"
-          />
-        </Button>
-
-        <Button onClick={navigateForward}>
-          <Image
-            src="/arrow-right.png"
-            alt="Right arrow"
-            width="40"
-            height="40"
-          />
-        </Button>
-      </ButtonNav>
     </Container>
   );
 };
