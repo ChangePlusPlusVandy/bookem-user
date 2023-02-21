baa4f11c3241f15460e693c52d5402cca67257a0
Zi Nean Teoh <zinean00@gmail.com>
2023-02-21 16:50:51 -0600

    clean up EventCard

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,125 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
 import Image from 'next/image';
+import { convertToDate, getTime } from 'utils/utils';
+import { EventType } from '@/types/types';
+import {
+  AddressContainer,
+  AddressIcon,
+  ClockIcon,
+  Container,
+  EventImage,
+  InfoContainer,
+  InfoFlex,
+  InfoFlexChild,
+  Name,
+} from '@/styles/components/eventCard.styles';
 
-interface RatioProp {
-  ratio: number;
-}
-
-type EventType = {
-  source: string;
-  name: string;
-  location: string;
-  date: Date;
-  time: string;
-  availability: number;
-  id: number;
-};
-
-const Container = styled.div<RatioProp>`
-  background-color: white;
-  max-width: 300px;
-  height: ${props => props.ratio * 350}px;
-  border-radius: 10px;
-  padding: ${props => props.ratio * 17}px;
-  margin: ${props => props.ratio * 17}px;
-  border: 1px solid black;
-`;
-
-const EventImage = styled.div<RatioProp>`
-  display: flex;
-  justify-content: center;
-  margin-top: ${props => props.ratio * 17}px;
-`;
-
-const Name = styled.div<RatioProp>`
-  background-color: white;
-  margin-top: ${props => props.ratio * 29}px;
-  height: ${props => props.ratio * 40.45}px;
-  font-style: normal;
-  font-weight: 700;
-  font-size: ${props => props.ratio * 18}px;
-  line-height: ${props => props.ratio * 22}px;
-`;
-
-const AddressContainer = styled.div<RatioProp>`
-  height: fit-content;
-  margin-top: ${props => props.ratio * 13}px;
-  background-color: white;
-  font-style: normal;
-  font-weight: 400;
-  font-size: ${props => props.ratio * 18}px;
-  line-height: ${props => props.ratio * 22}px;
-`;
-
-const AddressIcon = styled.div`
-  float: left;
-  margin-right: 8px;
-`;
-
-const InfoContainer = styled.div<RatioProp>`
-  height: fit-content;
-  margin-top: ${props => props.ratio * 13}px;
-  background-color: white;
-`;
-
-const ClockIcon = styled.div`
-  float: left;
-  margin-right: 8px;
-`;
-
-const InfoFlex = styled.div`
-  height: fit-content;
-  background-color: white;
-  display: flex;
-  gap: 5px;
-  justify-content: space-between;
-`;
-
-const InfoFlexChild = styled.div<RatioProp>`
-  background-color: white;
-  flex: 0 1 auto;
-  text-align: center;
-  vertical-align: middle;
-  font-style: normal;
-  font-weight: 400;
-  font-size: ${props => props.ratio * 18}px;
-  line-height: ${props => props.ratio * 22}px;
-`;
-
+// EventCard specific implementation of sizeMap
 const sizeMap = new Map<string, number>([
   ['large', 328 / 328],
   ['medium', 300 / 328],
   ['small', 250 / 328],
 ]);
 
-const toRatio = (size: string): number => {
+// helper method for converting size to its corresponding ratio
+const toRatio = (size: 'large' | 'medium' | 'small'): number => {
   let ratio = sizeMap.get(size);
   if (ratio !== undefined) return ratio;
   else return 1;
 };
-
-function convertToDate(date: Date) {
-  return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
-}
-
-function getTime(date: Date) {
-  var ampm: String = 'AM';
-  var hours: Number;
-
-  if (date.getHours() == 0) {
-    hours = 12;
-  } else {
-    if (date.getHours() > 12) {
-      ampm = 'PM';
-      hours = date.getHours() - 12;
-    } else {
-      hours = date.getHours();
-    }
-  }
-
-  return hours + ':' + date.getMinutes().toString() + ' ' + ampm;
-}
 
 const EventCard = ({
   eventData,
@@ -127,10 +34,15 @@ const EventCard = ({
 }: {
   // TODO: change type of eventData
   eventData: EventType;
+  // specify the size of the EventCard
   size: 'large' | 'medium' | 'small';
 }) => {
+  // convert the event date to Date format
   const eventDate = new Date(eventData.date);
+
+  // get ratio based on size to be used in computing distances
   const ratio = toRatio(size);
+
   return (
     <Container ratio={ratio}>
       <EventImage ratio={ratio}>
@@ -158,7 +70,7 @@ const EventCard = ({
       <InfoContainer ratio={ratio}>
         <ClockIcon>
           <Image
-            src="/dateicon.png"
+            src="/date-icon.png"
             alt="Clock icon"
             width={`${Math.round(ratio * 21.27)}`}
             height={`${Math.round(ratio * 22.14)}`}
