2154392fb7cce31b61d2121cca2f60feafe25efe
leozhang6 <leo.t.zhang@gmail.com>
2023-02-21 19:34:55 -0600

    extracted leftdisplay styles

--- components/LeftDisplay.tsx
+++ components/LeftDisplay.tsx
@@ -2,41 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 50vw;
-  height: 100vh;
-  background: #dbdbdb;
-  padding: 50px 40px;
-  overflow-y: auto;
-`;
-
-const ImgContainer = styled.div`
-  position: relative;
-  width: 85%;
-  height: 60vw;
-  min-height: 200px;
-  min-width: 120px;
-  margin: 0 auto;
-`;
-
-const HeaderFont = styled.p`
-  font-size: 25px;
-  margin: 4px;
-  width: 85%;
-  font-weight: bold;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const InfoFont = styled.p`
-  font-size: 15px;
-  width: 85%;
-  margin-top: 0px;
-  margin-left: auto;
-  margin-right: auto;
-`;
+import {
+  Container,
+  ImgContainer,
+  HeaderFont,
+  InfoFont,
+} from '@/styles/leftdisplay.styles';
 
 const LeftDisplay = () => {
   return (
