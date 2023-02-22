44ca3b097e078ac3a3ff6737cdd25352f1e38899
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2023-02-21 19:22:52 -0600

    Removed unnecessary css in futureEvents.styles

--- styles/components/futureEvents.styles.tsx
+++ styles/components/futureEvents.styles.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: white;
   width: 100%;
   height: 100%;
 `;
@@ -12,8 +11,6 @@ export const NavHeader = styled.div`
   padding: 26px 60px;
   display: flex;
   align-items: center;
-  margin: 0 auto;
-  background-color: white;
 `;
 
 export const NavLeft = styled.div`
@@ -23,7 +20,6 @@ export const NavLeft = styled.div`
 export const NavRight = styled.div`
   position: relative;
   width: 50%;
-  justify-content: end;
   text-align: right;
   img {
     cursor: pointer;
@@ -44,7 +40,6 @@ export const Input = styled.input`
   color: #262626;
   padding: 7px 33px;
   border-radius: 15px;
-  color: black;
   cursor: text;
   font-size: 14px;
   font-weight: 300;
@@ -53,57 +48,10 @@ export const Input = styled.input`
   width: 92%;
   margin: auto;
 
-  background-image: url(search-icon.png);
-  background-repeat: no-repeat;
-  background-size: 25px;
-  background-position: 5px;
-
   &:active,
   &:focus {
     text-align: left;
   }
-`;
-
-export const ImgContainer = styled.div`
-  position: relative;
-  flex-basis: 50%;
-  flex-basis: calc(20.333%);
-  margin: 40px;
-  cursor: pointer;
-  align-items: center;
-`;
-
-export const ImgIcons = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #ffffff;
-  margin-right: 20px;
-
-  svg {
-    margin-right: 10px;
-  }
-`;
-
-export const ImgMeta = styled.div`
-  display: none;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  background-color: rgba(0, 0, 0, 0.5);
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-`;
-
-export const Img = styled.img`
-  cursor: pointer;
-  width: 100%;
-  padding: 80px;
-  padding-top: 20px;
-  border-radius: 7px;
-  border: 1px solid #6b6b6b;
 `;
 
 export const ImagesWrapper = styled.div`
