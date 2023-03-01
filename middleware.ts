26f4f08d38d7b17d1e48093c57f1bd7b866a81f2
Zi Nean Teoh <zinean00@gmail.com>
2023-03-01 00:18:38 -0600

    remove middleware function bc it overrides default export

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from 'next/server';
-
 /**
  * THIS FILE HAS TO STAY HERE!
  * Use the default next-auth middleware pattern.
@@ -7,9 +5,6 @@ import { NextResponse } from 'next/server';
  * to redirect them to the sign-in page.
  */
 export { default } from 'next-auth/middleware';
-
-// TODO: figure out why this is necessary
-export async function middleware(req: any, ev: any) {}
 
 /**
  * Configure which api routes to authenticate
