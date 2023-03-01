10a3a2ff8a90251a7f79feb15274163162947c4e
Carol He <carolhe.che.17@gmail.com>
2023-02-28 19:07:20 -0600

    refactor registerFlow css

--- styles/registerFlow.styles.tsx
+++ styles/registerFlow.styles.tsx
@@ -4,16 +4,15 @@ export const DotsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-right: 120px;
-  margin-left: 120px;
+  margin: auto 120px;
 `;
 
 export const DotsFlex = styled.div`
+  position: absolute;
+  transform: translate(0%, 83.3%);
   display: flex;
   justify-content: space-between;
   width: 84px;
-  position: absolute;
-  transform: translate(0%, 83.3%);
 `;
 
 export const LeftArrow = styled.div`
@@ -25,9 +24,9 @@ export const RightArrow = styled.div`
 `;
 
 export const ProgressContainer = styled.div`
+  position: absolute;
+  bottom: 0;
   left: 50%;
   transform: translate(-50%, -50%);
   padding-bottom: 5vh;
-  position: absolute;
-  bottom: 0;
 `;
