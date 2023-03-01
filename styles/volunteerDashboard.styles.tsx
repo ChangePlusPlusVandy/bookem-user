f52716ad54af513a7e9e270f400ff65dc4e97be4
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2023-02-28 19:46:26 -0600

    Fix button styling on volunteer dashboard

--- styles/volunteerDashboard.styles.tsx
+++ styles/volunteerDashboard.styles.tsx
@@ -18,9 +18,9 @@ export const LogButton = styled.button`
   background: #6d6d6d;
   border-radius: 10px;
   width: 350px;
-  height: 88px;
+  height: 74px;
   padding: 19px;
-  padding-top: 25px;
+  margin-top: 25px;
   font-style: normal;
   font-weight: 400;
   font-size: 30px;
@@ -38,7 +38,7 @@ export const LogButton = styled.button`
  */
 export const ButtonIcon = styled.div`
   float: left;
-  margin-right: 40px;
+  margin: 5px 40px 0 0;
 `;
 
 /**
@@ -46,16 +46,16 @@ export const ButtonIcon = styled.div`
  */
 export const HistoryButton = styled.button`
   width: 350px;
-  height: 88px;
+  height: 74px;
   padding: 19px;
-  padding-top: 25px;
+  margin-top: 25px;
   background: #e3e3e3;
   border-radius: 10px;
   font-style: normal;
   font-weight: 400;
   font-size: 30px;
   line-height: 36px;
-  color: #000000;
+  border-color: #d4d4d4;
   display: flex;
   align-items: center;
   &:hover {
