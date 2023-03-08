ed9e3a6a4408a29f14b016aaafd7bed5802f1f4e
Zi Nean Teoh <zinean00@gmail.com>
2023-03-07 23:01:03 -0600

    install dotenv and setup cypress baseURL

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: process.env.NEXTAUTH_URL,
   },
 });
