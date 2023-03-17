4b008e216404035844ee727ca0e83c7902c2d1b6
Zi Nean Teoh <zinean00@gmail.com>
2023-03-17 16:32:50 -0500

    setup responsive rendering for mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { Container, MainContent } from '@/styles/layout.styles';
 import { Sidebar } from '@/components/Sidebar/Sidebar';
 import { SessionProvider } from 'next-auth/react';
+import { Media, MediaContextProvider } from '@/lib/media';
 import type { AppProps } from 'next/app';
 
 export default function App({
@@ -22,9 +23,17 @@ export default function App({
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Container>
-      <Sidebar />
-      <MainContent>{children}</MainContent>
-    </Container>
+    <MediaContextProvider disableDynamicMediaQueries>
+      <Media lessThan="sm">
+        {/* TODO: add layout for mobile */}
+        <div>Hello mobile</div>
+      </Media>
+      <Media greaterThanOrEqual="sm">
+        <Container>
+          <Sidebar />
+          <MainContent>{children}</MainContent>
+        </Container>
+      </Media>
+    </MediaContextProvider>
   );
 };
