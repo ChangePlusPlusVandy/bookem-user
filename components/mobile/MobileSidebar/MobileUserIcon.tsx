1f6a7e4a2b99ddd4a9679f5d50ec572a8c15f68f
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-03-23 22:21:07 -0500

    Display User Icon

--- components/mobile/MobileSidebar/MobileUserIcon.tsx
+++ components/mobile/MobileSidebar/MobileUserIcon.tsx
@@ -1,9 +1,16 @@
-import { ImageContainer } from '@/styles/components/DesktopSidebar/userIcon.styles';
-
+import {
+  ImageContainer,
+  Name,
+} from '@/styles/components/mobile/MobileSidebar/userIcon.styles';
+import Image from 'next/image';
 export const MobileUserIcon = () => {
   return (
     <>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        {/* TODO: add user profile image */}
+        <Image src="/pretty.png" width={70} height={70} alt="" />
+      </ImageContainer>
+      <Name>Linda S.</Name>{' '}
     </>
   );
 };
--- styles/components/mobile/MobileSidebar/userIcon.styles.tsx
+++ styles/components/mobile/MobileSidebar/userIcon.styles.tsx
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+/**
+ * Circular header image container
+ */
+export const ImageContainer = styled.div`
+  width: 70px;
+  height: 70px;
+  background-color: white;
+`;
+
+export const Name = styled.div`
+  margin: auto;
+  color: white;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 30px;
+`;
