a5049d65e315275dc6d3c99867046bfe56d16bdc
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-03-25 17:07:31 -0500

    Delete useless stuffs

--- styles/components/mobile/MobileSidebar/sidebar.styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-/**
- * Container of sidebar
- */
-export const Container = styled.div`
-  position: relative;
-  width: 100%;
-  background-color: pink;
-`;
