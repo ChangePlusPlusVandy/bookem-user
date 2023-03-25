3b1559214a47198b297f8200784f4e1af03eae9f
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-03-25 15:07:13 -0500

    restructuring

--- components/DesktopSidebar/DesktopSidebar.tsx
+++ components/DesktopSidebar/DesktopSidebar.tsx
@@ -6,6 +6,7 @@ import {
   IconLink,
   Container,
   IconText,
+  MobileIconsContainer,
 } from '@/styles/components/DesktopSidebar/sidebar.styles';
 import {
   SIDEBAR_ICON_HEIGHT,
@@ -29,49 +30,99 @@ export const DesktopSidebar = () => {
       <Media lessThan="sm">
         <UserIcon />
       </Media>
+
       {/* Iterate through iconParamList to display icons */}
       {SIDEBAR_ICON_PARAMS.map(iconParam => {
         return (
-          <IconContainer key={iconParam.desktopDefaultSrc}>
-            {/* Link that wraps around the icon */}
-            <IconLink
-              href={iconParam.linkTo}
-              hoveredsrc={iconParam.desktopHoveredSrc}
-              // Dynamically assign the background color according to the current route
-              backgroundcolor={
-                activeRoute === iconParam.linkTo ? '#d9d9d9' : '#6d6d6d'
-              }
-              // Dynamically assign the src of the icon according to the current route
-              imgsrc={
-                activeRoute === iconParam.linkTo
-                  ? iconParam.desktopHoveredSrc
-                  : iconParam.desktopDefaultSrc
-              }>
-              {/* Desktop version only displays image */}
-              <Media greaterThanOrEqual="sm">
-                {/* Icon image with default src */}
-                <Image
-                  src={iconParam.desktopDefaultSrc}
-                  alt=""
-                  width={SIDEBAR_ICON_HEIGHT}
-                  height={SIDEBAR_ICON_WIDTH}
-                />
-              </Media>
+          <Media greaterThanOrEqual="sm" key={iconParam.linkTo}>
+            <IconContainer>
+              {/* Link that wraps around the icon */}
+              <IconLink
+                href={iconParam.linkTo}
+                hoveredsrc={iconParam.desktopHoveredSrc}
+                // Dynamically assign the background color according to the current route
+                backgroundcolor={
+                  activeRoute === iconParam.linkTo ? '#d9d9d9' : '#6d6d6d'
+                }
+                // Dynamically assign the src of the icon according to the current route
+                imgsrc={
+                  activeRoute === iconParam.linkTo
+                    ? iconParam.desktopHoveredSrc
+                    : iconParam.desktopDefaultSrc
+                }>
+                {/* Desktop version only displays image */}
+                <Media greaterThanOrEqual="sm">
+                  {/* Icon image with default src */}
+                  <Image
+                    src={iconParam.desktopDefaultSrc}
+                    alt=""
+                    width={SIDEBAR_ICON_HEIGHT}
+                    height={SIDEBAR_ICON_WIDTH}
+                  />
+                </Media>
 
-              {/* Mobile version displays image + text */}
-              <Media lessThan="sm">
-                <Image
-                  src={iconParam.desktopDefaultSrc}
-                  alt=""
-                  width={SIDEBAR_ICON_HEIGHT}
-                  height={SIDEBAR_ICON_WIDTH}
-                />
-                <IconText>{iconParam.text}</IconText>
-              </Media>
-            </IconLink>
-          </IconContainer>
+                {/* Mobile version displays image + text */}
+                <Media lessThan="sm">
+                  <Image
+                    src={iconParam.desktopDefaultSrc}
+                    alt=""
+                    width={SIDEBAR_ICON_HEIGHT}
+                    height={SIDEBAR_ICON_WIDTH}
+                  />
+                  <IconText>{iconParam.text}</IconText>
+                </Media>
+              </IconLink>
+            </IconContainer>
+          </Media>
         );
       })}
+
+      <Media lessThan="sm">
+        <MobileIconsContainer>
+          {SIDEBAR_ICON_PARAMS.map(iconParam => {
+            return (
+              <IconContainer key={iconParam.linkTo}>
+                {/* Link that wraps around the icon */}
+                <IconLink
+                  href={iconParam.linkTo}
+                  hoveredsrc={iconParam.desktopHoveredSrc}
+                  // Dynamically assign the background color according to the current route
+                  backgroundcolor={
+                    activeRoute === iconParam.linkTo ? '#d9d9d9' : '#6d6d6d'
+                  }
+                  // Dynamically assign the src of the icon according to the current route
+                  imgsrc={
+                    activeRoute === iconParam.linkTo
+                      ? iconParam.desktopHoveredSrc
+                      : iconParam.desktopDefaultSrc
+                  }>
+                  {/* Desktop version only displays image */}
+                  <Media greaterThanOrEqual="sm">
+                    {/* Icon image with default src */}
+                    <Image
+                      src={iconParam.desktopDefaultSrc}
+                      alt=""
+                      width={SIDEBAR_ICON_HEIGHT}
+                      height={SIDEBAR_ICON_WIDTH}
+                    />
+                  </Media>
+
+                  {/* Mobile version displays image + text */}
+                  <Media lessThan="sm">
+                    <Image
+                      src={iconParam.desktopDefaultSrc}
+                      alt=""
+                      width={SIDEBAR_ICON_HEIGHT}
+                      height={SIDEBAR_ICON_WIDTH}
+                    />
+                    <IconText>{iconParam.text}</IconText>
+                  </Media>
+                </IconLink>
+              </IconContainer>
+            );
+          })}
+        </MobileIconsContainer>
+      </Media>
     </Container>
   );
 };
