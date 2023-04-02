1aad339b1ee48d36c0e18edf954826f360ad96e7
Zi Nean Teoh <zinean00@gmail.com>
2023-04-01 22:22:50 -0500

    move dummy data to where it is used

--- components/Home/PastActivity.tsx
+++ components/Home/PastActivity.tsx
@@ -1,7 +1,31 @@
 import React, { Suspense } from 'react';
-import { dummyEventData } from '@/components/Home/UpcomingEvents';
 const EventCard = React.lazy(() => import('@/components/shared/EventCard')); // implement lazy loading
 import { Container, Header } from '@/styles/components/pastActivity.styles';
+import mongoose from 'mongoose';
+import { QueriedVolunteerProgramData } from 'bookem-shared/src/types/database';
+
+/**
+ * Dummy data for event cards
+ */
+const dummyEventData: QueriedVolunteerProgramData = {
+  _id: new mongoose.Types.ObjectId(),
+  name: 'Distribute books (BNFK)',
+  description: 'blablabla',
+  schools: [],
+  programDate: new Date('2005-12-17T13:24:00'),
+  category: 'RFR',
+  isOpen: true,
+  volunteers: [],
+  maxSpot: 11,
+  location: {
+    street: '3593 Cedar Rd',
+    city: 'Nashville',
+  },
+  phone: '123-456-7890',
+  email: 'test_user@bookem.com',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
 // vertical list of sample PastEvents
 const PastActivity = () => {
