a64031c33fec4cda2cfb2906440b3e6f68eb8344
Zi Nean Teoh <zinean00@gmail.com>
2023-04-03 14:44:23 -0500

    convert program to events

--- components/Event/EventName.tsx
+++ components/Event/EventName.tsx
@@ -1,51 +1,42 @@
 import { QueriedVolunteerEventData } from 'bookem-shared/src/types/database';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   EventNameBox,
   NameAndSpot,
-  SignupButton,
+  Status,
+  StatusBox,
 } from '@/styles/components/Event/eventName.styles';
 import { useSession } from 'next-auth/react';
+import { SignupButton } from '@/styles/components/Event/event.styles';
+import { Media } from '@/lib/media';
+import Image from 'next/image';
+
+/**
+ * Calculate the length of the event volunteers
+ * If event.volunteers is undefined, return 0
+ */
+export const getEventLength = (event: QueriedVolunteerEventData) => {
+  if (event.volunteers && event.volunteers.length)
+    return event.volunteers.length;
+  else return 0;
+};
 
 /**
  * Contain the Event name and sign up button
  * @param event
  */
-const EventName = ({ event }: { event: QueriedVolunteerEventData }) => {
-  const [signedUp, setSignedUp] = useState(false);
+const EventName = ({
+  signedUp,
+  setSignedUp,
+  event,
+  signUpEvent,
+}: {
+  signedUp: boolean;
+  setSignedUp: (signedUp: boolean) => void;
+  event: QueriedVolunteerEventData;
+  signUpEvent: () => void;
+}) => {
   const { data: session } = useSession();
-
-  /**
-   * Sign up/Unsign up the current user to the event
-   * @param event
-   */
-  const signUpEvent = async () => {
-    try {
-      // If the event is not open, users need to submit an application
-      if (!event.requireApplication) {
-        // TODO: redirect to event application page
-        alert('Go to event application!');
-        return;
-      }
-
-      const response = await fetch('/api/event/' + event._id, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      // Success
-      if (response.status === 200) {
-        const message = await response.json();
-        console.log(message);
-        // Update sign up state
-        setSignedUp(!signedUp);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   useEffect(() => {
     // Initialize signedUp according to whether the current event
@@ -53,30 +44,35 @@ const EventName = ({ event }: { event: QueriedVolunteerEventData }) => {
     if (session?.user) {
       setSignedUp(event.volunteers.includes(session.user._id));
     }
-  }, [event.volunteers, session?.user]);
-
-  /**
-   * Calculate the length of the event volunteers
-   * If event.volunteers is undefined, return 0
-   */
-  const getEventLength = () => {
-    if (event.volunteers && event.volunteers.length)
-      return event.volunteers.length;
-    else return 0;
-  };
+  }, [event.volunteers, session?.user, setSignedUp]);
 
   return (
     <EventNameBox>
-      <NameAndSpot>
-        {/* TODO: get the name of the tags rather the mongodb id. do this after tag management is complete */}
-        <b>{event.name}</b>
-        {event.tags.length > 0 && <p>({event.program.tagName})</p>}
-        <br />
-        {getEventLength()}/{event.maxSpot} spots filled
-      </NameAndSpot>
-      <SignupButton onClick={signUpEvent}>
-        <span>{signedUp ? 'Signed up' : 'Sign up'}</span>
-      </SignupButton>
+      {/* Desktop */}
+      <Media greaterThanOrEqual="sm">
+        <NameAndSpot>
+          <b>{event.name}</b>
+          {event.tags.length > 0 && <p>({event.program.tagName})</p>}
+          <br />
+          {getEventLength(event)}/{event.maxSpot} spots filled
+        </NameAndSpot>
+        <SignupButton onClick={signUpEvent}>
+          <span>{signedUp ? 'Signed up' : 'Sign up'}</span>
+        </SignupButton>
+      </Media>
+
+      {/* Mobile */}
+      <Media lessThan="sm">
+        <NameAndSpot>
+          <b>{event.name}</b> <span>({event.program.tagName}) </span>
+          <br />
+          <StatusBox>
+            <Image src="/event/dot.svg" alt="" width={10} height={10} />
+            {/* TODO: display this dynamically after database schema is updated */}
+            <Status>{'ongoing'}</Status>
+          </StatusBox>
+        </NameAndSpot>
+      </Media>
     </EventNameBox>
   );
 };
--- components/Event/Footer.tsx
+++ components/Event/Footer.tsx
@@ -1,43 +1,43 @@
 import { SignupButton } from '@/styles/components/Event/event.styles';
-import { QueriedVolunteerProgramData } from 'bookem-shared/src/types/database';
+import { QueriedVolunteerEventData } from 'bookem-shared/src/types/database';
 import { useSession } from 'next-auth/react';
 import React, { useEffect } from 'react';
-import { getProgramLength } from './ProgramName';
+import { getEventLength } from './EventName';
 import { MaxSpot, SignupBox } from '@/styles/components/Event/footer.styles';
 
 /**
  * Contains spots filled and Sign Up button in Mobile
  * @param signedUp
  * @param setSignedUp
- * @param program
+ * @param event
  * @param signUpEvent
  */
 const Footer = ({
   signedUp,
   setSignedUp,
-  program,
+  event,
   signUpEvent,
 }: {
   signedUp: boolean;
   setSignedUp: (signedUp: boolean) => void;
-  program: QueriedVolunteerProgramData;
+  event: QueriedVolunteerEventData;
   signUpEvent: () => void;
 }) => {
   // Get user id in session
   const { data: session } = useSession();
 
   useEffect(() => {
-    // Initialize signedUp according to whether the current program
+    // Initialize signedUp according to whether the current event
     // contains the user or not
     if (session?.user) {
-      setSignedUp(program.volunteers.includes(session.user._id));
+      setSignedUp(event.volunteers.includes(session.user._id));
     }
-  }, [program.volunteers, session?.user, setSignedUp]);
+  }, [event.volunteers, session?.user, setSignedUp]);
   return (
     <>
       <SignupBox>
         <MaxSpot>
-          {getProgramLength(program)}/{program.maxSpot} spots filled
+          {getEventLength(event)}/{event.maxSpot} spots filled
         </MaxSpot>
         <SignupButton onClick={signUpEvent}>
           <span>{signedUp ? 'Signed up' : 'Sign up'}</span>
