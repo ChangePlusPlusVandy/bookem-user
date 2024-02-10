136b7088f5f8b9ae8d1162f1a2da8f84100419a1
David Huang <davidhuang1203@hotmail.com>
2024-02-10 16:26:42 -0600

    toggle middleware behavior for API testing without logging in

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
+import { testingAPI } from './utils/api-testing';
+
 /**
  * THIS FILE HAS TO STAY HERE!
  * Use the default next-auth middleware pattern from https://next-auth.js.org/configuration/nextjs
@@ -9,12 +11,18 @@ export { default } from 'next-auth/middleware';
 /**
  * Configure which api routes to authenticate
  */
-export const config = {
-  // match all routes in /api except for the route /api/users/create
-  // the ?!(...) means match everything except for (...)
-  // adapted from: https://nextjs.org/docs/messages/invalid-route-source
-  matcher: ['/api/((?!users/create).*)'],
-};
+// when in development, export an empty config
+export const config = testingAPI
+  ? {} // Don't use authentication for tests
+  : {
+      // match all routes in /api except for the route /api/users/create
+      // the ?!(...) means match everything except for (...)
+      // adapted from: https://nextjs.org/docs/messages/invalid-route-source
+      matcher: ['/api/((?!users/create).*)'],
+    };
 
-// Comment everything and uncomment this to test
-// export function middleware() {}
+function middleware() {}
+// when in development, export the empty middleware function
+if (testingAPI) {
+  module.exports = { ...module.exports, middleware };
+}
---
+++ utils/api-testing.ts
@@ -0,0 +1,15 @@
+// common utils for simple API testing in development phase
+
+// change this var to true when you are testing the API endpoints without logging in
+export const testingAPI = false;
+
+const fakeUserSession = {
+  user: {
+    id: '65ae7bbd24dc37492f2581c0',
+  },
+};
+
+// returns a fake user session when testing the API
+export const makeSessionForAPITest = () => {
+  return testingAPI ? fakeUserSession : null;
+};
